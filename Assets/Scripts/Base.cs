3a8dae2db6e374bb2e8c8419195b92c5d903cce4
Chris Brandt <chris@topher.games>
2020-08-08 11:59:34 -0500

    Some work on the bases

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    private int curHealth;
+
+    private void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    public Vector2 GetPosition() => transform.position;
+
+    public void TakeHit()
+    {
+        if(--curHealth <= 0)
+            BlowUp();
+
+        // change sprite based on % health remaining
+    }
+
+    public void Restore()
+    {
+        curHealth = maxHealth;
+        // full health sprite
+        // maybe restore particle effect
+    }
+
+    private void BlowUp()
+    {
+        // sprite change
+        // explosion effect
+        BaseManager.Inst.BaseDestroyed(this);
+    }
+}
---
+++ Assets/Scripts/BaseManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseManager : MonoBehaviour
+{
+    public static BaseManager Inst;
+
+    private Base[] bases;
+    private List<Base> activeBases = new List<Base>();
+
+    private Vector2 emptyPos;
+
+    private void Awake() => Inst = this;
+
+    private void Start()
+    {
+        bases = FindObjectsOfType<Base>();
+        activeBases.AddRange(bases);
+
+        emptyPos = transform.Find("EmptyPos").position;
+    }
+
+    public Vector2 GetRandomActiveBase()
+    {
+        if(activeBases.Count == 0)
+        {
+            // Signal Game Over
+            return emptyPos;
+        }
+
+        var @base = activeBases[Random.Range(0, activeBases.Count)];
+        return @base.GetPosition();
+    }
+
+    public void BaseDestroyed(Base @base)
+    {
+        activeBases.Remove(@base);
+
+        if(activeBases.Count == 0)
+            ; // signal game over
+    }
+
+    public void BaseRestored(Base @base)
+    {
+        activeBases.Add(@base);
+    }
+}
