e8e9fbf7468c076faa440516213f35f5d5553164
Chris Brandt <chris@topher.games>
2020-08-08 09:19:02 -0500

    Base
    New Unity project + TMPro + Maybe layout?

---
+++ Assets/Scripts/Problem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Problem : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI overheadText = null;
+
+    [SerializeField]
+    private string tempText = "x = 1 + 1";
+
+    private void OnEnable()
+    {
+        overheadText.text = tempText;
+    }
+}
