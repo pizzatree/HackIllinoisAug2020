648e8fd3500c549928b971eb75d15eb16a12bf87
Chris Brandt <chris@topher.games>
2020-08-08 12:53:20 -0500

    Missile Basics & Prefab variants of enemy

---
+++ Assets/EnemyMissile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMissile : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Vector2 target;
+
+    private void Start()
+    {
+        target = BaseManager.Inst.GetRandomActiveBase();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
